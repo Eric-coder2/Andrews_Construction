28296cb1a29cb512a935c1937f7027c42f06d157
Eric Penkala <ericpenkalate@gmail.com>
2026-08-03 17:21:11 -0400

    fixed FAQ animation

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 const allDetails = document.querySelectorAll("details");
 
 allDetails.forEach((details) => {
-  details.open = true; // keeps content rendered permanently
+  details.open = true;
 
   const summary = details.querySelector("summary");
+  const wrapper = details.querySelector(".faq-answer-wrapper");
 
   summary.addEventListener("click", (event) => {
     event.preventDefault();
     details.classList.toggle("is-open");
+
+    if (details.classList.contains("is-open")) {
+      wrapper.style.maxHeight = wrapper.scrollHeight + "px";
+    } else {
+      wrapper.style.maxHeight = "0px";
+    }
   });
 });
